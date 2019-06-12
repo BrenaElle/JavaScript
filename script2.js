66e48e70b4bf0787694c5c0b9ebbd237befd247c
BrenaElle <brena.ellen@academico.ifpb.edu.br>
2019-06-12 14:44:39 -0300

    Novos Exercícios

---
+++ script2.js
@@ -0,0 +1,24 @@
+
+
+//wile=(enquanto).
+
+x = 0;
+
+while(x < 10){
+
+    document.write("</br> O valor do x é : "  + x);
+    
+    x++;
+}
+
+x=5;
+
+document.write("</br></br></br> O X esta valendo: " + x + "</br></br>");
+
+//for = Para
+
+for(a = 0; a < x; a++){
+
+     document.write("</br> O Valor do A: " + a);
+
+}
